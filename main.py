03754a9ae773fcdfafd3b170ae6237686852ce42
jamullins <114003827+JaMullins@users.noreply.github.com>
2025-12-11 14:20:47 -0500

    Refactor motor setup and control logic in main.py
    Updated motor configurations and added new control logic for intake and swivel mechanisms. Improved variable naming conventions and added comments for clarity.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# ---------------------------------------------------------------------------- #
-#                                                                              #
-#   Module:       main.py                                                      #
-#   Author:       alancalderonaguilar                                          #
-#   Created:      10/23/2025, 3:02:46 PM                                       #
-#   Description:  V5 project                                                   #
-#                                                                              #
-# ---------------------------------------------------------------------------- #
+### Capital variables are GLOBAL, and must be global imported into each newly defined method as per python syntax.
+### Do NOT delete ANY of the instantiated variables, create a new one if required and comment out the old one.
+### Comment on any changed code and promptly upload it to GitHub at the end of each meeting.
+### Do NOT use a main() method as the driver, it does not allow for commenting out easily for testing.
+### Any other preferences or rules can be added here if needed. That is all.
 
 # Library imports
 import time
@@ -23,7 +20,7 @@ FWD = True
 # Put radio in port 11  
 halfMotor = Motor(Ports.PORT9,GearSetting.RATIO_18_1,FWD)
 
-motor1 = Motor(Ports.PORT1,GearSetting.RATIO_6_1,FWD)
+motor1 = Motor(Ports.PORT13,GearSetting.RATIO_6_1,FWD)
 motor2 = Motor(Ports.PORT10,GearSetting.RATIO_6_1,REV)
 motor3 = Motor(Ports.PORT19,GearSetting.RATIO_6_1,FWD)
 motor4 = Motor(Ports.PORT20,GearSetting.RATIO_6_1,REV)
@@ -32,21 +29,25 @@ intakeMotor1 = Motor(Ports.PORT7,GearSetting.RATIO_18_1,REV)
 intakeMotor2 = Motor(Ports.PORT6,GearSetting.RATIO_18_1,FWD)
 
 intakeSwivel = Motor(Ports.PORT5,GearSetting.RATIO_36_1,FWD)
+intakeSwivel.set_stopping(HOLD)
+intakeSwivel.reset_position()
 
 inertial = Inertial(Ports.PORT8)
 inertial.calibrate()
 wait(200,MSEC)
 
 
-spinning = False
-swivelling = False
+SPINNING = False
+SWIVELLING = False
+SWIVELDIR = False # False for REV, True for FWD
+SPINDIR = False
 
 
 def testDiagonol():
-    global spinning, swivelling
+    global SPINNING, SWIVELLING, SWIVELDIR
     # Goal for this is to test how well the half motor can keep up with 1-2 regular motors if it was a supplemental sideways force
     # This would allow for diagonol movement that mimicks true X-drive
-    while not time.sleep(0.2):
+    while not time.sleep(0.1):
         sideways_motion = (controller.axis4.position())*2
         forward_motion = controller.axis3.position()
         forward_motion_decimal = float(forward_motion/100)
@@ -72,35 +73,59 @@ def testDiagonol():
         intakeSwivel.set_velocity(35,PERCENT)
 
 
-        if spinning:
-            intakeMotor1.spin(FORWARD)
-            intakeMotor2.spin(FORWARD)
-        elif not spinning:
+        if SPINNING:
+            if SPINDIR:
+                intakeMotor1.spin(FORWARD)
+                intakeMotor2.spin(FORWARD)
+            elif not SPINDIR:
+                intakeMotor1.spin(REVERSE)
+                intakeMotor2.spin(REVERSE)
+        elif not SPINNING:
             intakeMotor2.stop()
             intakeMotor1.stop()
-
-        if swivelling:
-            intakeSwivel.spin(REVERSE)
-        elif not spinning:
-            intakeSwivel.stop()
+        
+        if SWIVELDIR:
+                intakeSwivel.spin_to_position(-100,wait=False)
+        elif not SWIVELDIR:
+                intakeSwivel.spin_to_position(0,DEGREES)
 
         
 
-
-        def test():
-            global spinning
-            spinning = not spinning
+        def spIntake():
+            global SPINNING
+            SPINNING = not SPINNING
+        
+        def inputBuffer():
+            time.sleep(.1)
 
 
         def swivel():
-            global swivelling
-            swivelling = not swivelling
+            global SWIVELLING
+            SWIVELLING = not SWIVELLING
+
+        def spOutake():
+            global SPINDIR
+            SPINDIR = not SPINDIR
+        
+        def swivelDir():
+            global SWIVELDIR
+            SWIVELDIR = not SWIVELDIR
+            
             
             
         if controller.buttonR1.pressing():
-            test()
+            spIntake()
+            inputBuffer()
         if controller.buttonR2.pressing():
             swivel()
+            inputBuffer()
+        if controller.buttonL1.pressing():
+            spOutake()
+            inputBuffer()
+        if controller.buttonL2.pressing():
+            swivelDir()
+            inputBuffer()
+
 
 
 
